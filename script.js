c693e34f716a46d11f8870b4841fab4ca63f708c
GjorgievFilip <filipgjorgiev1999@gmail.com>
2026-02-27 16:55:13 +0100

    Added starting functions for JS

--- script.js
+++ script.js
@@ -1,4 +1,75 @@
 //Constants
 const path = window.location.pathname;
 
-console.log("Hello Quantum Computers!");
+//Pages
+
+function IndexPage()
+{
+    console.log("Index Page");
+}
+
+function ApplicationsPage()
+{
+    console.log("Applications Page");
+}
+
+function PowerPage()
+{
+    console.log("Power Page");
+}
+
+function SecurityPage()
+{
+    console.log("Security Page");
+}
+
+function ArtificalIntelligencePage()
+{
+    console.log("Artificial Intelligence Page");
+}
+
+function ModelsPage()
+{
+    console.log("Models Page");
+}
+
+function GalleryPage()
+{
+    console.log("Gallery Page");
+}
+//Pages
+
+if (path.endsWith("index.html"))
+{
+    IndexPage();
+}
+
+if (path.endsWith("applications.html"))
+{
+    IndexPage();
+}
+
+if (path.endsWith("power.html"))
+{
+    IndexPage();
+}
+
+if (path.endsWith("security.html"))
+{
+    IndexPage();
+}
+
+if (path.endsWith("ai.html"))
+{
+    IndexPage();
+}
+
+if (path.endsWith("models.html"))
+{
+    IndexPage();
+}
+
+if (path.endsWith("gallery.html"))
+{
+    IndexPage();
+}
